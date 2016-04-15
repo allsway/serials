a9c3791d6abc90f57ee833eb89e8af6e26cb148f
Sarina <ssinick@calstate.edu>
2016-04-14 18:08:18 -0700

    Update getnextpredicteditem.sql

--- getnextpredicteditem.sql
+++ getnextpredicteditem.sql
@@ -1,12 +1,12 @@
-SELECT id2reckey(item_record_id) as "RECORD #(ITEM)",
-id2reckey(order_record_metadata_id) as "RECORD #(ORDER)",
-id2reckey(bib_record_holding_record_link.bib_record_id) as "RECORD #(BIBLIO)",
-id2reckey(bib_record_holding_record_link.holding_record_id) as "RECORD #(HOLDING)",
+SELECT id2reckey(item_record_id) AS "RECORD #(ITEM)",
+id2reckey(order_record_metadata_id) AS "RECORD #(ORDER)",
+id2reckey(bib_record_holding_record_link.bib_record_id) AS "RECORD #(BIBLIO)",
+id2reckey(bib_record_holding_record_link.holding_record_id) AS "RECORD #(HOLDING)",
 claimon_date_gmt - INTERVAL '1 day' * claim_days AS "Expected Date",
-'0000-00-00' as "Received Date",
-marc_tag as "MARC LINK",
-tag as "SUBFIELD",
-field_content as "PATTERN"
+'0000-00-00' AS "Received Date",
+marc_tag AS "MARC LINK",
+tag AS "SUBFIELD",
+field_content AS "PATTERN"
 FROM sierra_view.bib_record_item_record_link 
 LEFT JOIN sierra_view.bib_record_holding_record_link ON bib_record_holding_record_link.bib_record_id=bib_record_item_record_link.bib_record_id
 LEFT JOIN sierra_view.holding_record ON bib_record_holding_record_link.holding_record_id = 
@@ -15,9 +15,14 @@ LEFT JOIN sierra_view.holding_record_card ON holding_record.id=
 holding_record_card.holding_record_id
 LEFT JOIN sierra_view.holding_record_cardlink ON holding_record_cardlink.holding_record_card_id=holding_record_card.id
 INNER JOIN 
+    (SELECT record_id AS rid FROM sierra_view.subfield_view  
+    WHERE
+    marc_tag = '853' AND tag = '8' AND marc_ind1 = '9' GROUP BY record_id,marc_tag,tag) AS corrects
+ON holding_record.id=corrects.rid
+INNER JOIN 
     (SELECT max(content) AS field_content,record_id,marc_tag,tag FROM sierra_view.subfield_view  
     WHERE
     marc_tag = '853' AND tag = '8' AND marc_ind1 = '' GROUP BY record_id,marc_tag,tag) AS subfields
 ON holding_record.id=subfields.record_id
-WHERE  order_record_metadata_id is not null and
+WHERE  order_record_metadata_id IS NOT NULL AND
  item_record_id=reckey2id('i3657178');
